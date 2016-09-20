f2c7b61235bcb9757fcf4b0e85c4a9edf8a9f5a7
romarioremmy123 <romarioremmy154@gmail.com>
2016-09-20 18:28:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,33 +5,36 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-    res.send("");
+    res.send(`<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="center">
+            <img src="/ui/madi.png" class="img-medium"/>
+        </div>
+        <br>
+        <div class="center text-big bold">
+            Hi! I am Romario Remmy.
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>`);
 });
 
 app.get('/article-two', function (req, res) {
  
-  res.send('<!doctype html>'+
-   '<html>'+
-    '<head>'+
-        '<link href="/ui/style.css" rel="stylesheet" />'+
-    '</head>'+
-    '<body>'+
-        '<div class="center">'+
-            '<img src="/ui/madi.png" class="img-medium"/>'+
-        '</div>'+
-        '<br>'+
-        '<div class="center text-big bold">'+
-            'Hi! I am Romario Remmy.'+
-        '</div>'+
-        '<script type="text/javascript" src="/ui/main.js">'+
-        '</script>'+
-    '</body>'+
-'</html>'+'');
+  res.send();
 });
 
 app.get('/article-three', function (req, res) {

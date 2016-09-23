3031f2adb499e813ed72ebec629afc26cb6ef631
romarioremmy123 <romarioremmy154@gmail.com>
2016-09-23 22:51:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,10 @@
 console.log('Loaded!');
+var margin = 0;
 var element = document.getElementById("romy");
 element.onclick = function() {
     var interval = setInterval(moveRight, 100);
 };
-var margin = 0;
+
 var moveLeft = function() {
     margin+=10;
     element.style.marginLeft(margin+"px");

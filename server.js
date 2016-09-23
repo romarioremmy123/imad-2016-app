bbb7a31810f91a7d340c289b2f27550f68ec67ba
romarioremmy123 <romarioremmy154@gmail.com>
2016-09-23 23:08:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
 app.get('/article-one', function (req, res) {
     res.send(`<!doctype html>
@@ -51,6 +48,10 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/Romario.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Romario.jpg'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 

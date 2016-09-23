269562d67d407ffefea337b8ce995a5d39dff8fd
romarioremmy123 <romarioremmy154@gmail.com>
2016-09-23 22:57:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/article-one', function (req, res) {
     res.send(`<!doctype html>
 <html>
